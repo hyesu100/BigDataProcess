fe13215ae132cf98f6029d29358df6fafe4eff54
hyesu100 <bhs9166@gmail.com>
2022-11-01 01:59:50 +0900

    UBER

--- HW3/UBERStudent20180601.py
+++ HW3/UBERStudent20180601.py
@@ -5,15 +5,24 @@ import calendar
 input_file = sys.argv[1] 
 output_file = sys.argv[2]
 
-def day_of_the_week(date):
-    days = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
-    day = date.weekday()
-    return(days[day])
-
 trip = dict()
+DayOfTheWeek = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
 
 with open(input_file, "rt") as fp:
     for line in fp:
-		uber = line.split(",")
+		uber = line.strip().split(",")
         uber_date = uber[1].split("/")
-        today = day_of_the_week(date(int(uber_date[2]), int(uber_date[0]), int(uber_date[1])))
+        today = calendar.weekday(int(uber_date[2]), int(uber_date[0]), int(uber_date[1]))
+        today = DayOfTheWeek[today]
+		info = (uber[0], today)
+
+
+        if today not in trip:
+            trip[today] = [int(uber[2]), int(uber[3])]
+        else:
+            trip[today][0] += int(today[2])
+			trip[today][1] += int(today[3])
+
+with open(output_file, "wt") as fp:
+    for key, value in trip.items():
+        fp.write(key + " " + str(value[0]) + str(value[1]) + "\n")
