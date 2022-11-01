6fef297353d29c29fa2fb9dcdbac6480e223e9d7
hyesu100 <bhs9166@gmail.com>
2022-11-02 02:20:18 +0900

    HW3 Fix

--- HW3/UBERStudent20180601.py
+++ HW3/UBERStudent20180601.py
@@ -5,25 +5,27 @@ from datetime import datetime, date
 input_file = sys.argv[1] 
 output_file = sys.argv[2]
 
+dicOne = dict()
+dicTwo = dict()
+
 def DayOfTheWeek(date):
 	days = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
 	day = date.weekday()
 	return (days[day])
 
-trip = dict()
 with open(input_file, "rt") as fp:
     for line in fp:
         uber = line.split(",")
-        uberDate = uber[1].split("/")
-        today = DayOfTheWeek(date(int(uberDate[2]), int(uberDate[0]), int(uberDate[1])))
+        uberDay = uber[1].split("/")
+        today = uberRegion + "," + DayOfTheWeek(date(int(uberDay[2]), int(uberDay[0]), int(uberDay[1])))
 
-        if today not in trip:
-            trip[today] = int(uber[2])
-            trip[today] = int(uber[3])
+        if today not in dicOne:
+            dicOne[today] = int(uber[2])
+            dicTwo[today] = int(uber[3])
         else:
-            trip[today][0] += int(uber[2])
-            trip[today][1] += int(uber[3])
+            dicOne[today][0] += int(uber[2])
+            dicTwo[today][1] += int(uber[3])
 
 with open(output_file, "wt") as fp:
-    for key, value in trip.items():
+    for key, value in dicOne.items():
         fp.write(key + " " + str(value[0]) + str(value[1]) + "\n")
