4cb698eebc6b4d6329a9764cb04f33d7f10c3561
hyesu100 <bhs9166@gmail.com>
2022-11-01 16:18:37 +0900

    UBER

--- HW3/UBERStudent20180601.py
+++ HW3/UBERStudent20180601.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 import sys
-import calendar
+from datetime import datetime, date
 
 input_file = sys.argv[1] 
 output_file = sys.argv[2]
 
-DayOfTheWeek = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
+def DayOfTheWeek(date):
+	days = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
+	day = date.weekday()
+	return (days[day])
 
 trip = dict()
 with open(input_file, "rt") as fp:
     for line in fp:
-        line = line.strip()
         uber = line.split(",")
         uberDate = uber[1].split("/")
-        today = calendar.weekday(int(uberDate[2]), int(uberDate[0]), int(uberDate[1]))
-        today = uberDate[0] + DayOfTheWeek[today]
+        today = DayOfTheWeek(date(int(uberDate[2]), int(uberDate[0]), int(uberDate[1])))
 
         if today not in trip:
             trip[today] = [int(uber[2]), int(uber[3])]
