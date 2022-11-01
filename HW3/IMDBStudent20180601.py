a45c579d0acc11e920f708f36588f178a6a0f235
hyesu100 <bhs9166@gmail.com>
2022-11-01 09:32:37 +0900

    IMDB Fix

--- HW3/IMDBStudent20180601.py
+++ HW3/IMDBStudent20180601.py
@@ -7,15 +7,15 @@ output_file = sys.argv[2]
 genres = dict()
 with open(input_file, "rt") as fp:
     for row in fp:
-        movies = row.split("::")
-        movies_genre = list(movies[2].strip())
-        genre = list(movies_genre[2].split("|"))
+            rows = row.strip()
+            movies = row.split("::")
+            genre = movies[2].split("|")
 
-        for g in genre:
-            if g not in genres:
-                genres[g] = 1
-            else:
-                genres[g] += 1
+            for g in genre:
+                if g not in genres:
+                    genres[g] = 1
+                else:
+                    genres[g] += 1
 
 with open(output_file, "wt") as fp:
     for key, value in genres.items():
