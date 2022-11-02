51f024b3121b90254bf444de6683da47aa8b09fe
hyesu100 <bhs9166@gmail.com>
2022-11-02 11:29:34 +0900

    HW3

--- HW3/UBERStudent20180601.py
+++ HW3/UBERStudent20180601.py
@@ -2,8 +2,6 @@
 import sys
 from datetime import datetime, date
 
-from numpy import append
-
 input_file = sys.argv[1] 
 output_file = sys.argv[2]
 
@@ -12,26 +10,28 @@ def DayOfTheWeek(date):
 	day = date.weekday()
 	return week[day]
 
-Dict = dict()
-u = []
-d = []
+dictOne = dict()
+dictTwo = dict()
 
-with open(input_file, "rt") as fp:
-    for row in fp:
-        uber = row.strip().split(",")
-        uber = u.append(uber)
-        uberDay = int(uber[1].split("/"))
-        uberDay = d.append(uberDay)
-        today = uber[0] + "," + DayOfTheWeek(date(uberDay[2], uberDay[0], uberDay[1]))
+with open(input_file, "rt") as f:
+	for row in f:
+		uber = row.split(",")
+		uber[-1] = uber[-1].split("\n")[0]
+		uberDay = uber[1].split("/")
+		uber[1] = DayOfTheWeek(date(int(uberDay[2]), int(uberDay[0]), int(uberDay[1])))
+		result = uber[0] + "," + uber[1]
 
-        if today not in Dict:
-            Dict[today] = int(uber[2])
-            Dict[today] = int(uber[3])
-        else:
-            Dict[today][0] += int(uber[2])
-            Dict[today][1] += int(uber[3])
+		if result not in dictOne:
+			dictOne[result] = int(uber[2])	
+			dictTwo[result] = int(uber[3])	
+
+		else:
+			dictOne[result] += int(uber[2])	
+			dictTwo[result] += int(uber[3])	
+
+string = dictOne.keys()
 
 with open(output_file, "wt") as fp:
-    for key, value in Dict.items():
-        fp.write(key + " " + str(value[0]) + "," + str(value[1]) + "\n")
+	for key in dictOne.keys():
+		f.write(k +" "+str(dictOne[k]) + "," + str(dictTwo[k]) + "\n")
 
