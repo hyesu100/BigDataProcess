17647990bbdb473e1f7a2026a566b560ed1dffa5
hyesu100 <bhs9166@gmail.com>
2022-12-27 02:02:38 +0900

    HW4

---
+++ HW4/Student20180601.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+import sys
+import operator
+import numpy as np
+from os import listdir
+
+
+def createDataSet(dirname):
+    labels = []
+    trainingFileList = listdir(dirname)
+    m = len(trainingFileList)
+    matrix = np.zeros((m, 1024))
+
+    for i in range(m): 
+        fileNameStr = trainingFileList[i]
+        answer = int(fileNameStr.split('_')[0]) 
+        labels.append(answer)
+        matrix[i, :] = getVector(dirname + '/' + fileNameStr)
+    return matrix, labels
+
+    for i in range(m): 
+        fileNameStr = trainingFileList[i]
+        answer = int(fileNameStr.split('_')[0])
+        labels.append(answer)
+        matrix[i, :] = getVector(dirname + '/' + fileNameStr)
+    return matrix, labels
+
+
+def classify0(inX, dataSet, labels, k): 
+    dataSetSize = dataSet.shape[0]
+    diffMat = np.tile(inX, (dataSetSize, 1)) - dataSet
+    sqDiffMat = diffMat ** 2 
+    sqDistances = sqDiffMat.sum(axis = 1) 
+    distances = sqDistances ** 0.5 
+    sortedDistIndicies = distances.argsort() 
+    classCount = {}
+
+    for i in range(k): 
+            voteIlabel = labels[sortedDistIndicies[i]] 
+            classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1 
+    sortedClassCount = sorted(classCount.items(), key= operator.itemgetter(1), reverse=True) 
+    return sortedClassCount[0][0]
+
+
+def getVector(filename):  
+    vector = np.zeros((1, 1024)) 
+    with open(filename) as f:
+        for i in range(32):
+            line = f.readline()
+            for j in range(32):
+                vector[0, 32 * i + j] = int(line[j])
+        return vector        
